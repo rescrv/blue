93be3ba8094e042b98b2a15d086b3c202d3d19af
Robert Escriva <robert@rescrv.net>
2022-12-30 15:49:34 -0500

    [bloom] A tool to compute optimal bloom filter configurations.

---
+++ bloom/src/bin/bloom.rs
@@ -0,0 +1,60 @@
+use clap::{App, Arg, ArgMatches};
+
+use bloom::{calc_keys_given_probability, calc_m_given_p_n, calc_p_given_n_m, Parameter, N, P, M, K};
+
+fn parse<X: Parameter>(args: &ArgMatches, name: &str) -> Option<X> {
+    let x = match args.value_of(name) {
+        Some(x) => x,
+        None => {
+            return None;
+        },
+    };
+    Some(X::new(x.parse::<f64>().expect("could not parse value")))
+}
+
+fn main() {
+    let app = App::new("bloom")
+        .version("0.1.0")
+        .about("compute bloom filter parameters");
+    let app = app.arg(
+        Arg::with_name("n")
+            .long("n")
+            .takes_value(true)
+            .help("Expected number of elements to insert into the bloom filter."));
+    let app = app.arg(
+        Arg::with_name("p")
+            .long("p")
+            .takes_value(true)
+            .help("Probability of a false positive."));
+    let app = app.arg(
+        Arg::with_name("m")
+            .long("m")
+            .takes_value(true)
+            .help("Number of bits to provide the bloom filter."));
+    let app = app.arg(
+        Arg::with_name("k")
+            .long("k")
+            .takes_value(true)
+            .help("Number of hash functions/keys to set in the bloom filter."));
+    let args = app.get_matches();
+    let n = parse::<N>(&args, "n");
+    let p = parse::<P>(&args, "p");
+    let m = parse::<M>(&args, "m");
+    let k = parse::<K>(&args, "k");
+    match (n, p, m, k) {
+        (None, Some(p), None, None) => {
+            println!("a bloom filter with false positive rate of {} will work best with {} keys", p.0, calc_keys_given_probability(p).0);
+        }
+        (Some(n), Some(p), None, None) => {
+            println!("a bloom filter for {} items with false positive rate {} will need {} bits", n.0, p.0, calc_m_given_p_n(p, n).0);
+        }
+        (Some(n), None, Some(m), None) => {
+            println!("a bloom filter for {} items with {} bits will have a {} false positive rate", n.0, m.0, calc_p_given_n_m(n, m).0);
+        }
+        (_, _, _, _) => {
+            println!("provide --p to compute --k");
+            println!("provide --p --n to compute --m");
+            println!("provide --n --m to compute --p");
+        }
+    }
+}
---
+++ bloom/src/lib.rs
@@ -0,0 +1,112 @@
+//////////////////////////////////////////// parameters ////////////////////////////////////////////
+
+pub trait Parameter {
+    fn new(x: f64) -> Self;
+}
+
+const LN2_2: f64 = 0.4804530139182014; // ln^2 2
+
+#[derive(Clone, Copy)]
+pub struct N(pub f64);
+
+impl Parameter for N {
+    fn new(x: f64) -> N {
+        N(x)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct P(pub f64);
+
+impl Parameter for P {
+    fn new(x: f64) -> P {
+        P(x)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct M(pub f64);
+
+impl Parameter for M {
+    fn new(x: f64) -> M {
+        M(x)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct K(pub f64);
+
+impl Parameter for K {
+    fn new(x: f64) -> K {
+        K(x)
+    }
+}
+
+/////////////////////////////////////////// Calculations ///////////////////////////////////////////
+
+pub fn calc_keys_given_probability(p: P) -> K {
+    K(0.0f64 - p.0.log2())
+}
+
+pub fn calc_m_given_p_n(p: P, n: N) -> M {
+    M(0.0 - n.0 * p.0.ln() / LN2_2)
+}
+
+pub fn calc_p_given_n_m(n: N, m: M) -> P {
+    let e = std::f64::consts::E;
+    P(e.powf(0.0 - LN2_2 * m.0 / n.0))
+}
+
+/////////////////////////////////////////////// tests //////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestParams {
+		n: N,
+		p: P,
+		m: M,
+		k: K,
+    }
+
+    const TEST_PARAMS: &[TestParams] = &[
+		TestParams {n: N(100.0), p: P(0.01), m: M(958.505837736744), k: K(6.643856189774724)},
+		TestParams {n: N(1000.0), p: P(0.001), m: M(14377.58756605116), k: K(9.965784284662087)},
+		TestParams {n: N(2718281.0), p: P(0.0314159), m: M(19578296.19763294), k: K(4.99236127889529)},
+    ];
+
+    fn approximately_correct(expected: f64, returned: f64) -> bool {
+        returned * 0.99 < expected && returned * 1.01 > expected
+    }
+
+    #[test]
+    fn ln2_2() {
+        let expect = 2.0f64.ln().powf(2.0);
+        assert!(approximately_correct(expect, LN2_2));
+    }
+
+    #[test]
+    fn test_keys_given_probability() {
+        for param in TEST_PARAMS.iter() {
+            let k = calc_keys_given_probability(param.p);
+            assert!(approximately_correct(param.k.0, k.0));
+        }
+    }
+
+    #[test]
+    fn test_m_given_p_n() {
+        for param in TEST_PARAMS.iter() {
+            let m = calc_m_given_p_n(param.p, param.n);
+            assert!(approximately_correct(param.m.0, m.0));
+        }
+    }
+
+    #[test]
+    fn test_p_given_n_m() {
+        for param in TEST_PARAMS.iter() {
+            let p = calc_p_given_n_m(param.n, param.m);
+            assert!(approximately_correct(param.p.0, p.0));
+        }
+    }
+}
