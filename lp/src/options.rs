7beebce86cdd6f60e6c7a64ff5163a8fe4e9f8f8
Robert Escriva <robert@rescrv.net>
2023-01-21 20:06:19 -0800

    [lp] Have sst_options as CompactionOptions

--- lp/src/options.rs
+++ lp/src/options.rs
@@ -1,3 +1,4 @@
+use super::sst::SSTBuilderOptions;
 use super::TABLE_FULL_SIZE;
 
 ///////////////////////////////////////// CompactionOptions ////////////////////////////////////////
@@ -5,6 +6,7 @@ use super::TABLE_FULL_SIZE;
 #[derive(Clone, Debug)]
 pub struct CompactionOptions {
     pub max_compaction_bytes: u64,
+    pub sst_options: SSTBuilderOptions,
 }
 
 impl CompactionOptions {
@@ -19,6 +21,7 @@ impl Default for CompactionOptions {
     fn default() -> Self {
         Self {
             max_compaction_bytes: TABLE_FULL_SIZE as u64 * 128,
+            sst_options: SSTBuilderOptions::default(),
         }
     }
 }
