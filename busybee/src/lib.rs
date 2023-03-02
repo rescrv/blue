af9e8ddbfd8cfa92726da3c23282d24e8e6a373e
Robert Escriva <robert@rescrv.net>
2023-03-02 12:27:37 -0800

    [busybee] Very slim stubs.

---
+++ busybee/src/lib.rs
@@ -0,0 +1,43 @@
+extern crate prototk_derive;
+
+use std::ops::Deref;
+use std::sync::Mutex;
+
+use biometrics::click;
+
+use buffertk::{stack_pack, Buffer, Packable, Unpackable};
+
+use prototk::Error as ProtoTKError;
+use prototk::field_types::*;
+use prototk_derive::Message;
+
+////////////////////////////////////////////// Context /////////////////////////////////////////////
+
+pub struct Context {
+}
+
+////////////////////////////////////////// ResponseHolder //////////////////////////////////////////
+
+pub trait ResponseHolder {
+}
+
+/////////////////////////////////////////////// Error //////////////////////////////////////////////
+
+pub enum Error {
+}
+
+///////////////////////////////////////////// RPCServer ////////////////////////////////////////////
+
+pub trait RPCServer {
+    type Error: Packable;
+
+    fn call(&self, ctx: Context, func: String, req: &[u8]) -> Result<Buffer, Self::Error>;
+}
+
+///////////////////////////////////////////// RPCClient ////////////////////////////////////////////
+
+pub trait RPCClient {
+    type Error<'a>: Unpackable<'a>;
+
+    fn call<'a>(&self, ctx: Context, func: String, req: &[u8]) -> Result<Buffer, Self::Error<'a>>;
+}
