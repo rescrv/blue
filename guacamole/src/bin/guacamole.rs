910f78dd6f13f7546b8e328fb3af7ac899940518
Robert Escriva <robert@rescrv.net>
2023-05-30 15:41:42 -0700

    [guacamole] Switch to arrrg.

---
+++ guacamole/src/bin/guacamole.rs
@@ -0,0 +1,31 @@
+use std::io::Write;
+
+use arrrg::CommandLine;
+use arrrg_derive::CommandLine;
+
+use guacamole::Guacamole;
+
+#[derive(CommandLine, Default)]
+struct GuacamoleCommandLine {
+    #[arrrg(optional, "Number of bytes to generate before exiting.  By default, 1<<64.", "N")]
+    bytes: Option<u64>,
+    #[arrrg(optional, "Guacamole seed.")]
+    seed: Option<u64>,
+}
+
+/// Generate pseudo-random, predictable bytes.
+fn main() {
+    let cmdline = GuacamoleCommandLine::from_command_line();
+    let mut guac = Guacamole::new(cmdline.seed.unwrap_or(0));
+    let mut remain = cmdline.bytes.unwrap_or(u64::max_value()) as usize;
+    let mut buf = [0u8; 1 << 20];
+    let buf: &mut [u8] = &mut buf;
+    while remain > 0 {
+        let amt = if remain > buf.len() { buf.len() } else { remain };
+        guac.generate(&mut buf[..amt]);
+        std::io::stdout()
+            .write_all(&buf[..amt])
+            .expect("failed to write");
+        remain -= amt;
+    }
+}
---
+++ guacamole/src/bin/zipf.rs
@@ -0,0 +1,51 @@
+use arrrg::CommandLine;
+use arrrg_derive::CommandLine;
+
+use guacamole::zipf::Zipf;
+use guacamole::Guacamole;
+
+#[derive(CommandLine)]
+struct ZipfCommandLine {
+    #[arrrg(required, "Approximate cardinality of the set.", "N")]
+    card: u64,
+    #[arrrg(optional, "Alpha value for the zipf distribution.")]
+    alpha: Option<f64>,
+    #[arrrg(optional, "Theta value for the zipf distribution.")]
+    theta: Option<f64>,
+    #[arrrg(optional, "Guacamole seed.")]
+    seed: Option<u64>,
+}
+
+impl Default for ZipfCommandLine {
+    fn default() -> Self {
+        Self {
+            card: 1000,
+            alpha: None,
+            theta: Some(0.99),
+            seed: None,
+        }
+    }
+}
+
+/// Choose numbers [0, n) from a zipf distribution.
+fn main() {
+    let cmdline = ZipfCommandLine::from_command_line();
+    if cmdline.alpha.is_none() && cmdline.theta.is_none() {
+        panic!("provide at least one of --alpha or --theta");
+    }
+    if cmdline.alpha.is_some() && cmdline.theta.is_some() {
+        panic!("provide at most one of --alpha or --theta");
+    }
+    let zipf = if let Some(alpha) = cmdline.alpha {
+        Zipf::from_alpha(cmdline.card, alpha)
+    } else if let Some(theta) = cmdline.theta {
+        Zipf::from_theta(cmdline.card, theta)
+    } else {
+        unreachable!();
+    };
+    let mut guac = Guacamole::new(cmdline.seed.unwrap_or(0));
+    loop {
+        let x = zipf.next(&mut guac);
+        println!("{}", x);
+    }
+}
