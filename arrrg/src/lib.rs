6fa6d29ead9c3536c122ae979d1f3ee913caf0c1
Robert Escriva <robert@rescrv.net>
2023-05-30 14:50:47 -0700

    [arrrg]  Convert bloom-calculator.
    I had to write some helper functions for arrrg to parse from the
    commandline or a set of arguments.  That's bundled too.

--- arrrg/src/lib.rs
+++ arrrg/src/lib.rs
@@ -2,10 +2,53 @@ use std::str::FromStr;
 
 //////////////////////////////////////////// CommandLine ///////////////////////////////////////////
 
-pub trait CommandLine {
+pub trait CommandLine: Sized + Default {
     fn add_opts(&self, prefix: Option<&str>, opts: &mut getopts::Options);
     fn matches(&mut self, prefix: Option<&str>, matches: &getopts::Matches);
     fn canonical_command_line(&self, prefix: Option<&str>) -> Vec<String>;
+
+    fn from_command_line() -> Self {
+        let args: Vec<String> = std::env::args().collect();
+        let args: Vec<&str> = args.iter().map(AsRef::as_ref).collect();
+        Self::from_arguments(&args[1..])
+    }
+
+    fn from_command_line_relaxed() -> Self {
+        let args: Vec<String> = std::env::args().collect();
+        let args: Vec<&str> = args.iter().map(AsRef::as_ref).collect();
+        Self::from_arguments_relaxed(&args[1..])
+    }
+
+    fn from_arguments(args: &[&str]) -> Self {
+        let command_line = Self::from_arguments_relaxed(args);
+        let reconstructed_args = command_line.canonical_command_line(None);
+        if args != reconstructed_args {
+            panic!("non-canonical commandline specified:
+provided: {:?}
+expected: {:?}
+check argument order amongst other differences",
+                   &args, reconstructed_args);
+        }
+        command_line
+    }
+
+    fn from_arguments_relaxed(args: &[&str]) -> Self {
+        let mut command_line = Self::default();
+        let mut opts = getopts::Options::new();
+        opts.parsing_style(getopts::ParsingStyle::StopAtFirstFree);
+        opts.long_only(true);
+        command_line.add_opts(None, &mut opts);
+
+        let matches = match opts.parse(args) {
+            Ok(matches) => { matches },
+            Err(err) => {
+                panic!("could not parse command line: {}", err);
+            }
+        };
+        command_line.matches(None, &matches);
+
+        command_line
+    }
 }
 
 //////////////////////////////////////////// macro utils ///////////////////////////////////////////
---
+++ bloom/src/bin/bloom-calculator.rs
@@ -0,0 +1,58 @@
+use bloom::{
+    calc_keys_given_probability, calc_m_given_p_n, calc_p_given_n_m, Parameter, K, M, N, P,
+};
+
+use arrrg::CommandLine;
+use arrrg_derive::CommandLine;
+
+#[derive(CommandLine, Debug, Default)]
+struct Parameters {
+    #[arrrg(optional, "Expected number of elements to insert into the bloom filter.", "N")]
+    card: Option<f64>,
+    #[arrrg(optional, "Expected probability of a false positive.", "P")]
+    prob: Option<f64>,
+    #[arrrg(optional, "Number of bits in use in the bloom filter.", "M")]
+    bits: Option<f64>,
+    #[arrrg(optional, "Number of hash functions/keys to set in the bloom filter.", "K")]
+    keys: Option<f64>,
+}
+
+fn main() {
+    let params = Parameters::from_command_line_relaxed();
+
+    let n = params.card.map(N::new);
+    let p = params.prob.map(P::new);
+    let m = params.bits.map(M::new);
+    let k = params.keys.map(K::new);
+
+    match (n, p, m, k) {
+        (None, Some(p), None, None) => {
+            println!(
+                "a bloom filter with false positive rate of {} will work best with {} keys",
+                p.0,
+                calc_keys_given_probability(p).0
+            );
+        }
+        (Some(n), Some(p), None, None) => {
+            println!(
+                "a bloom filter for {} items with false positive rate {} will need {} bits",
+                n.0,
+                p.0,
+                calc_m_given_p_n(p, n).0
+            );
+        }
+        (Some(n), None, Some(m), None) => {
+            println!(
+                "a bloom filter for {} items with {} bits will have a {} false positive rate",
+                n.0,
+                m.0,
+                calc_p_given_n_m(n, m).0
+            );
+        }
+        (_, _, _, _) => {
+            println!("provide --prob to compute --keys");
+            println!("provide --card --prob to compute --bits");
+            println!("provide --card --bits to compute --prob");
+        }
+    }
+}
