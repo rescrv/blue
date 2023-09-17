ee7218955acb142c164f51e4ce54d10d062803b5
Robert Escriva <robert@rescrv.net>
2023-09-17 09:11:13 -0700

    [sync42] Create a monitor class.

---
+++ sync42/examples/monitor.rs
@@ -0,0 +1,141 @@
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::sync::{Arc, Condvar, MutexGuard};
+
+use biometrics::{Collector, Counter};
+
+use sync42::monitor::{Coordination, CriticalSection, Monitor};
+
+//////////////////////////////////////////// Biomentrics ///////////////////////////////////////////
+
+static WRITER_TAKES_IT: Counter = Counter::new("monitor.writer_takes_it");
+static WRITER_LEAVES_IT: Counter = Counter::new("monitor.writer_leaves_it");
+static WRITER_WAITING: Counter = Counter::new("monitor.writer_waiting");
+static RELEASE_NOTIFIES: Counter = Counter::new("monitor.release_notifies");
+
+fn register_biometrics(collector: &mut Collector) {
+    collector.register_counter(&WRITER_TAKES_IT);
+    collector.register_counter(&WRITER_LEAVES_IT);
+    collector.register_counter(&WRITER_WAITING);
+    collector.register_counter(&RELEASE_NOTIFIES);
+}
+
+////////////////////////////////////////////// Monitor /////////////////////////////////////////////
+
+struct WriteState<'a> {
+    wait_to_write: &'a Condvar,
+    to_write: Option<String>,
+    writes: Vec<String>,
+}
+
+struct CoalesceWrites {
+    writer_waiting: bool,
+    writer_has_it: bool,
+    writes: Vec<String>,
+}
+
+impl Coordination<WriteState<'_>> for CoalesceWrites {
+    fn acquire<'a>(mut guard: MutexGuard<'a, Self>, ws: &mut WriteState<'_>) -> (bool, MutexGuard<'a, Self>) {
+        loop {
+            if !guard.writer_has_it {
+                WRITER_TAKES_IT.click();
+                guard.writer_has_it = true;
+                std::mem::swap(&mut guard.writes, &mut ws.writes);
+                ws.writes.push(ws.to_write.take().unwrap());
+                return (true, guard);
+            } else if guard.writer_waiting {
+                WRITER_LEAVES_IT.click();
+                guard.writes.push(ws.to_write.take().unwrap());
+                return (false, guard);
+            } else {
+                WRITER_WAITING.click();
+                guard.writer_waiting = true;
+                guard = ws.wait_to_write.wait(guard).unwrap();
+                guard.writer_waiting = false;
+            }
+        }
+    }
+
+    fn release<'a>(mut guard: MutexGuard<'a, Self>, ws: &mut WriteState<'_>) -> MutexGuard<'a, Self> {
+        guard.writer_has_it = false;
+        if guard.writer_waiting {
+            RELEASE_NOTIFIES.click();
+            ws.wait_to_write.notify_one();
+        }
+        guard
+    }
+}
+
+struct WriteWithMutualExclusion {
+}
+
+impl WriteWithMutualExclusion {
+    fn do_write(&mut self, write: String) {
+        drop(write);
+    }
+}
+
+impl CriticalSection<WriteState<'_>> for WriteWithMutualExclusion {
+    fn critical_section(&mut self, ws: &mut WriteState) {
+        let mut writes = Vec::new();
+        std::mem::swap(&mut ws.writes, &mut writes);
+        for write in writes.into_iter() {
+            self.do_write(write);
+        }
+    }
+}
+
+////////////////////////////////////////////// Threads /////////////////////////////////////////////
+
+static COUNTER: AtomicU64 = AtomicU64::new(1);
+static DONE: AtomicBool = AtomicBool::new(false);
+static WAIT_TO_WRITE: Condvar = Condvar::new();
+
+fn worker_thread(monitor: Arc<Monitor<WriteState<'_>, CoalesceWrites, WriteWithMutualExclusion>>) {
+    while !DONE.load(Ordering::Relaxed) {
+        let num = COUNTER.fetch_add(1, Ordering::Relaxed);
+        let mut ws = WriteState {
+            wait_to_write: &WAIT_TO_WRITE,
+            to_write: Some(format!("seq_no={}", num)),
+            writes: Vec::new(),
+        };
+        monitor.do_it(&mut ws);
+    }
+}
+
+fn main() {
+    // Setup the environment.
+    std::thread::spawn(|| {
+        let mut collector = biometrics::Collector::new();
+        register_biometrics(&mut collector);
+        sync42::register_biometrics(&mut collector);
+        let fout = std::fs::File::create("/dev/stdout").unwrap();
+        let mut emit = biometrics::PlainTextEmitter::new(fout);
+        loop {
+            if let Err(e) = collector.emit(&mut emit) {
+                eprintln!("collector error: {}", e);
+            }
+            std::thread::sleep(std::time::Duration::from_millis(250));
+        }
+    });
+    // Build the monitor.
+    let coordination = CoalesceWrites {
+        writer_waiting: false,
+        writer_has_it: false,
+        writes: Vec::new(),
+    };
+    let critical_section = WriteWithMutualExclusion {};
+    let monitor = Arc::new(Monitor::new(coordination, critical_section));
+    // Spawn the theads.
+    let mut threads = Vec::new();
+    for _ in 0..64 {
+        let m = Arc::clone(&monitor);
+        threads.push(std::thread::spawn(move || {
+            worker_thread(m);
+        }));
+    }
+    std::thread::sleep(std::time::Duration::from_millis(60_000));
+    DONE.store(true, Ordering::Relaxed);
+    for thread in threads.into_iter() {
+        let _ = thread.join();
+    }
+}
---
+++ sync42/src/monitor.rs
@@ -0,0 +1,71 @@
+//! Implementation of a monitor in classic Hoare-style.
+//!
+//! A monitor allows for synchronization that is more concurrent than just using mutexes and
+//! condition variables.  Concretely, [Monitor] breaks the task into the coordination and the
+//! critical section.  Threads can concurrently coordinate the next-best thread to enter the
+//! citical section without blocking the critical section from executing.
+
+use std::cell::UnsafeCell;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Mutex, MutexGuard};
+
+/// Coordination tracks acquisition and release of the monitor.  It is an error that will induce
+/// panic to allow multiple threads to be between [`Coordination::acquire`] and [`Coordination::release`] calls.
+pub trait Coordination<T> {
+    /// Acquire the monitor.  Returns true if and only if the monitor can enter the critical
+    /// section.
+    fn acquire<'a>(guard: MutexGuard<'a, Self>, t: &mut T) -> (bool, MutexGuard<'a, Self>);
+    /// Release the monitor.  In a proper monitor [`Coordination::release`] will always follow a call to
+    /// acquire on the same thread.  Release is responsible for waking threads blocked in acquire.
+    fn release<'a>(guard: MutexGuard<'a, Self>, t: &mut T) -> MutexGuard<'a, Self>;
+}
+
+/// Critical section captures the mutually-exclusive section of the monitor.  The monitor ensures
+/// that there will only be one thread in the [`CriticalSection::critical_section`] at a time.
+pub trait CriticalSection<T> {
+    fn critical_section(&mut self, t: &mut T);
+}
+
+/// Monitor provides the monitor pattern.  It will synchronize calls to acquire and release and the
+/// critical section so that at most one thread is in the critical section at once.
+pub struct Monitor<T, COORD: Coordination<T>, CRIT: CriticalSection<T>> {
+    coordination: Mutex<COORD>,
+    synchronization: AtomicBool,
+    critical_section: UnsafeCell<CRIT>,
+    _t: std::marker::PhantomData::<T>,
+}
+
+impl<T, COORD: Coordination<T>, CRIT: CriticalSection<T>> Monitor<T, COORD, CRIT> {
+    /// Create a new monitor with the provided coordination and critical_section.
+    pub fn new(coordination: COORD, critical_section: CRIT) -> Self {
+        Self {
+            coordination: Mutex::new(coordination),
+            synchronization: AtomicBool::new(false),
+            critical_section: UnsafeCell::new(critical_section),
+            _t: std::marker::PhantomData,
+        }
+    }
+
+    /// Enter the monitor and call into the critical section if the call to
+    /// [`Coordination::acquire`] succeeds.
+    pub fn do_it(&self, t: &mut T) {
+        {
+            let coordination = self.coordination.lock().unwrap();
+            let (acquired, _coordination) = COORD::acquire(coordination, t);
+            if !acquired {
+                return;
+            }
+            if self.synchronization.swap(true, Ordering::Acquire) {
+                panic!("synchronization invariant violated: acquire should only allow one thread at a time in the critical section");
+            }
+        }
+        let crit: &mut CRIT = unsafe { &mut *self.critical_section.get() };
+        crit.critical_section(t);
+        let coordination = self.coordination.lock().unwrap();
+        self.synchronization.store(false, Ordering::Release);
+        drop(COORD::release(coordination, t));
+    }
+}
+
+unsafe impl<T, COORD: Coordination<T>, CRIT: CriticalSection<T>> Send for Monitor<T, COORD, CRIT> {}
+unsafe impl<T, COORD: Coordination<T>, CRIT: CriticalSection<T>> Sync for Monitor<T, COORD, CRIT> {}
