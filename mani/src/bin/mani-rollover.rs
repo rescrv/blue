b916c12749e17bfcb32facd6cee41939a698c87f
Robert Escriva <robert@rescrv.net>
2023-11-05 11:28:42 -0800

    [mani] fix mani-rollover usage string.

--- mani/src/bin/mani-rollover.rs
+++ mani/src/bin/mani-rollover.rs
@@ -3,7 +3,7 @@ use arrrg::CommandLine;
 use mani::{Manifest, ManifestOptions};
 
 fn main() {
-    let (options, roots) = ManifestOptions::from_command_line("USAGE: mani-append [OPTIONS] <root>");
+    let (options, roots) = ManifestOptions::from_command_line("USAGE: mani-rollover [OPTIONS] <root>");
     if roots.len() != 1 {
         eprintln!("must provide exactly one manifest root");
         std::process::exit(1);
