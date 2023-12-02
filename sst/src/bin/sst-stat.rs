1c33731311a921ade72fb70f0eb0bedc647923ec
Robert Escriva <robert@rescrv.net>
2023-12-01 19:02:03 -0800

    [sst] Tool to stat an sst.

---
+++ sst/src/bin/sst-stat.rs
@@ -0,0 +1,20 @@
+use arrrg::CommandLine;
+use arrrg_derive::CommandLine;
+
+use sst::setsum::Setsum;
+use sst::{Cursor, Sst, SstOptions};
+
+#[derive(CommandLine, Debug, Default, Eq, PartialEq)]
+struct SstStatOptions {
+    #[arrrg(nested)]
+    sst: SstOptions,
+}
+
+fn main() {
+    let (cmdline, args) =
+        SstStatOptions::from_command_line("Usage: sst-stat [OPTIONS] [SSTs]");
+    for path in args {
+        let sst = Sst::new(cmdline.sst.clone(), &path).expect("sst should open");
+        println!("{} {:?}", path, sst.metadata().expect("metadata call should succeed"));
+    }
+}
