f21c4a5e6ae257721841be257a0bb740dce55aec
Robert Escriva <robert@rescrv.net>
2023-12-26 20:18:20 +0000

    [tuple_key_derive] Do a docs pass.

--- tuple_key_derive/src/lib.rs
+++ tuple_key_derive/src/lib.rs
@@ -1,3 +1,4 @@
+#![doc = include_str!("../README.md")]
 #![recursion_limit = "128"]
 
 extern crate proc_macro;
@@ -11,6 +12,7 @@ use syn::{parse_macro_input, DeriveInput};
 
 ///////////////////////////////////// #[derive(TypedTupleKey)] /////////////////////////////////////
 
+/// Derive a TypedTupleKey. 
 #[proc_macro_derive(TypedTupleKey, attributes(tuple_key))]
 pub fn derive_typed_tuple_key(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
